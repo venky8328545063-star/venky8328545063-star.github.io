2fe3a062831ef8058f74c39b400041a2fdf43848
venky8328545063-star <venky8328545063@gmail.com>
2026-09-20 21:56:34 +0530

    Add config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const STORE = {
+  name: "Enduచేపలు",
+  tagline: "Authentic Andhra Dry Fish",
+  whatsappDisplay: "+91 81793 62070",
+  whatsappNumber: "918179362070",
+  callDisplay: "+91 83285 45063",
+  callNumber: "918328545063",
+  catalogUrl: "https://wa.me/c/918179362070",
+  paymentNote: "100% prepaid orders • COD not available",
+  location: "Andhra Pradesh, India"
+};
